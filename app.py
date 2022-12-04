9b1613c3bca167f8484c21ce5e807fdc55f48eca
parthin-12 <parthinbariya003@gmail.com>
2022-12-04 17:48:45 +0530

    first

--- app.py
+++ app.py
@@ -72,6 +72,6 @@ def getGlass(id):
         glasses[id] = glass_Object
     return glasses[id]
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(debug=True, port=port)
